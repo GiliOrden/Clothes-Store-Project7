f9d39e7a768c1e7824f7e8024a6090ebc5fe26c5
galiaorden@outlook.com <galiaorden@outlook.com>
2023-07-25 21:34:48 +0300

    the amount and the items almost ready

--- server/api.js
+++ server/api.js
@@ -172,50 +172,6 @@ app.put("/users", (req, res) => {
 });
 //####################     items        ##############################
 
-app.get("/items", (req, res) => {
-  const itemType = req.query.type;
-
-  if (!itemType) {
-    // If no 'type' parameter is provided, return all items
-    const query = "SELECT * FROM items";
-    con.query(query, (err, results) => {
-      if (err) {
-        console.error("Error in request execution", err);
-        return res
-          .status(500)
-          .send({ error: "An error occurred while fetching items." });
-      }
-
-      // If there are no items in the database, return an empty array
-      if (results.length === 0) {
-        return res.status(200).send([]);
-      }
-
-      // If items are found, send the items array in the response
-      res.status(200).send(results);
-    });
-  } else {
-    // If 'type' parameter is provided, filter items by the specified type
-    const query = "SELECT * FROM items WHERE type = ?";
-    con.query(query, [itemType], (err, results) => {
-      if (err) {
-        console.error("Error in request execution", err);
-        return res
-          .status(500)
-          .send({ error: "An error occurred while fetching items by type." });
-      }
-
-      // If there are no items of the specified type in the database, return an empty array
-      if (results.length === 0) {
-        return res.status(200).send([]);
-      }
-
-      // If items of the specified type are found, send the items array in the response
-      res.status(200).send(results);
-    });
-  }
-});
-
 // example: http://localhost:3001/items
 //get all the items in the db
 app.get("/items", (req, res) => {
@@ -238,6 +194,286 @@ app.get("/items", (req, res) => {
     res.status(200).send(results);
   });
 });
+//http://localhost:3001/items/type?type=Skirt
+app.get("/items/type", (req, res) => {
+  const itemType = req.query.type;
+
+  // If 'type' parameter is provided, filter items by the specified type
+  const query = "SELECT * FROM items WHERE type = ?";
+  con.query(query, [itemType], (err, results) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while fetching items by type." });
+    }
+
+    // If there are no items of the specified type in the database, return an empty array
+    if (results.length === 0) {
+      return res.status(200).send([]);
+    }
+
+    // If items of the specified type are found, send the items array in the response
+    res.status(200).send(results);
+  });
+});
+//get a specific item according to the id
+//http://localhost:3001/items/1
+app.get("/items/:item_id", (req, res) => {
+  const itemId = req.params.item_id;
+
+  const query = "SELECT * FROM items WHERE item_id = ?";
+  con.query(query, [itemId], (err, results) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while fetching the item." });
+    }
+
+    // If no item is found with the specified item_id, return a 404 status
+    if (results.length === 0) {
+      return res.status(404).send({ error: "Item not found." });
+    }
+
+    // If item is found, send the item object in the response
+    res.status(200).send(results[0]);
+  });
+});
+
+// {
+//     "item_description": "Updated Shirt",
+//     "type": "Shirt",
+//     "image": null
+//   }
+//http://localhost:3001/items/1
+//update item attributes ***** option for a manager****
+app.put("/items/:item_id", (req, res) => {
+  const itemId = req.params.item_id;
+  const newItem = req.body;
+
+  // Check if the 'item_id' parameter is a valid integer
+  if (isNaN(itemId) || parseInt(itemId) <= 0) {
+    return res
+      .status(400)
+      .send({ error: "Invalid item_id. Please provide a valid integer." });
+  }
+
+  // Validate that newItem contains at least one attribute to update
+  if (Object.keys(newItem).length === 0) {
+    return res
+      .status(400)
+      .send({ error: "Invalid request. No attributes provided for update." });
+  }
+
+  const updateQuery = "UPDATE items SET ? WHERE item_id = ?";
+  con.query(updateQuery, [newItem, itemId], (err, updateResult) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while updating the item." });
+    }
+
+    // Check if any rows were affected by the update
+    if (updateResult.affectedRows === 0) {
+      return res
+        .status(404)
+        .send({ error: "Item not found. No update performed." });
+    }
+
+    // Item successfully updated, fetch and return the updated item
+    const selectQuery = "SELECT * FROM items WHERE item_id = ?";
+    con.query(selectQuery, [itemId], (err, results) => {
+      if (err) {
+        console.error("Error in request execution", err);
+        return res.status(500).send({
+          error: "An error occurred while fetching the updated item.",
+        });
+      }
+
+      // If the updated item is found, send the item object in the response
+      res.status(200).send(results[0]);
+    });
+  });
+});
+
+//####################     amount        ##############################
+
+//http://localhost:3001/amount/1
+//gives all the sizes and there amount according a specific item id
+app.get("/amount/:item_id", (req, res) => {
+  const itemId = req.params.item_id;
+
+  // Check if the 'item_id' parameter is a valid integer
+  if (isNaN(itemId) || parseInt(itemId) <= 0) {
+    return res
+      .status(400)
+      .send({ error: "Invalid item_id. Please provide a valid integer." });
+  }
+
+  const query = "SELECT size, amount FROM amount WHERE item_id = ?";
+  con.query(query, [itemId], (err, results) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while fetching the item amounts." });
+    }
+
+    // If no amounts are found for the specified item_id, return a 404 status
+    if (results.length === 0) {
+      return res.status(404).send({ error: "Item amounts not found." });
+    }
+
+    // Item amounts found, send the amounts and sizes in the response
+    res.status(200).send(results);
+  });
+});
+// get the amount according the item id and a specific size
+//http://localhost:3001/amount/1/1
+app.get("/amount/:item_id/:size", (req, res) => {
+  const itemId = req.params.item_id;
+  const size = req.params.size;
+
+  // Check if the 'item_id' and 'size' parameters are valid integers
+  if (
+    isNaN(itemId) ||
+    parseInt(itemId) <= 0 ||
+    isNaN(size) ||
+    parseInt(size) <= 0
+  ) {
+    return res.status(400).send({
+      error: "Invalid item_id or size. Please provide valid integers.",
+    });
+  }
+
+  const query = "SELECT amount FROM amount WHERE item_id = ? AND size = ?";
+  con.query(query, [itemId, size], (err, results) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while fetching the item amount." });
+    }
+
+    // If no amount is found for the specified item_id and size, return a 404 status
+    if (results.length === 0) {
+      return res.status(404).send({ error: "Item amount not found." });
+    }
+
+    // Item amount found, send the amount in the response
+    res.status(200).send({ amount: results[0].amount });
+  });
+});
+//update the amount for a given item_id and size
+// http://localhost:3001/amount/1/1
+// {
+//   "amount": 20
+// }
+
+app.put("/amount/:item_id/:size", (req, res) => {
+  const itemId = req.params.item_id;
+  const size = req.params.size;
+  const updatedAmount = req.body.amount;
+
+  // Check if the 'item_id', 'size', and 'amount' parameters are valid integers
+  if (
+    isNaN(itemId) ||
+    parseInt(itemId) <= 0 ||
+    isNaN(size) ||
+    parseInt(size) <= 0 ||
+    isNaN(updatedAmount) ||
+    parseInt(updatedAmount) < 0
+  ) {
+    return res.status(400).send({
+      error: "Invalid item_id, size, or amount. Please provide valid integers.",
+    });
+  }
+
+  const updateQuery =
+    "UPDATE amount SET amount = ? WHERE item_id = ? AND size = ?";
+  con.query(updateQuery, [updatedAmount, itemId, size], (err, updateResult) => {
+    if (err) {
+      console.error("Error in request execution", err);
+      return res
+        .status(500)
+        .send({ error: "An error occurred while updating the item amount." });
+    }
+
+    // Check if any rows were affected by the update
+    if (updateResult.affectedRows === 0) {
+      return res
+        .status(404)
+        .send({ error: "Item amount not found. No update performed." });
+    }
+
+    // Item amount updated successfully
+    res
+      .status(200)
+      .send({ success: true, message: "Item amount updated successfully." });
+  });
+});
+
+//function for updating the amount. when adding to the cart we will get -1 in the amount. delete in the cart we will get +1 in the amount
+function updateAmount(itemId, size, operation) {
+  // Check if the 'item_id' and 'size' parameters are valid integers
+  if (
+    isNaN(itemId) ||
+    parseInt(itemId) <= 0 ||
+    isNaN(size) ||
+    parseInt(size) <= 0
+  ) {
+    return Promise.reject(
+      new Error("Invalid item_id or size. Please provide valid integers.")
+    );
+  }
+
+  // Check if the 'operation' parameter is valid
+  if (operation !== "+" && operation !== "-") {
+    return Promise.reject(
+      new Error(
+        "Invalid operation. Please provide '+' for increment or '-' for decrement."
+      )
+    );
+  }
+
+  const updateQuery = `UPDATE amount SET amount = amount ${
+    operation === "+" ? "+" : "-"
+  } 1 WHERE item_id = ? AND size = ?`;
+  return new Promise((resolve, reject) => {
+    con.query(updateQuery, [itemId, size], (err, updateResult) => {
+      if (err) {
+        console.error("Error in request execution", err);
+        reject(new Error("An error occurred while updating the item amount."));
+      } else {
+        // Check if any rows were affected by the update
+        if (updateResult.affectedRows === 0) {
+          reject(new Error("Item amount not found. No update performed."));
+        } else {
+          resolve("Item amount updated successfully.");
+        }
+      }
+    });
+  });
+}
+// // Assuming 'itemId' and 'size' are valid integers
+// updateAmount(itemId, size, '+')
+//   .then((message) => {
+//     console.log(message); // "Item amount updated successfully."
+//   })
+//   .catch((error) => {
+//     console.error(error.message); // Handle any errors here
+//   });
+
+// // Or for decrementing the amount
+// updateAmount(itemId, size, '-')
+//   .then((message) => {
+//     console.log(message); // "Item amount updated successfully."
+//   })
+//   .catch((error) => {
+//     console.error(error.message); // Handle any errors here
+//   });
 
 app.listen(3001, () => {
   console.log("app listening on port 3001.");
